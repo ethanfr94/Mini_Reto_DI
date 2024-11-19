1c6208388f812ec6ba9014e2177b8ae9dd857614
ethanfr94 <izan.francoanduaga@iesmiguelherrero.com>
2024-11-19 20:03:44 +0100

    Inicio interfac pagina principal

---
+++ MiniReto_interfaz_principal/MiniReto_interfaz_principal/Principal.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace MiniReto_interfaz_principal
+{
+    partial class Principal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            tsmiCiclos = new ToolStripMenuItem();
+            tsmiVerCiclos = new ToolStripMenuItem();
+            txmiProfesores = new ToolStripMenuItem();
+            tsmiVerProfesores = new ToolStripMenuItem();
+            tsmiModificarTutorias = new ToolStripMenuItem();
+            tsmiAlumnos = new ToolStripMenuItem();
+            tsmiVerAlumnos = new ToolStripMenuItem();
+            añadirAlumnoToolStripMenuItem = new ToolStripMenuItem();
+            modificarAlumnoToolStripMenuItem = new ToolStripMenuItem();
+            tsmiProyectos = new ToolStripMenuItem();
+            verProyectosToolStripMenuItem = new ToolStripMenuItem();
+            añadirProyectosToolStripMenuItem = new ToolStripMenuItem();
+            modificarProyectosToolStripMenuItem = new ToolStripMenuItem();
+            tsmiLogOut = new ToolStripMenuItem();
+            ltvListaPrincipal = new ListView();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { tsmiCiclos, txmiProfesores, tsmiAlumnos, tsmiProyectos, tsmiLogOut });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(722, 24);
+            menuStrip1.TabIndex = 7;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // tsmiCiclos
+            // 
+            tsmiCiclos.DropDownItems.AddRange(new ToolStripItem[] { tsmiVerCiclos });
+            tsmiCiclos.Name = "tsmiCiclos";
+            tsmiCiclos.Size = new Size(51, 20);
+            tsmiCiclos.Text = "Ciclos";
+            // 
+            // tsmiVerCiclos
+            // 
+            tsmiVerCiclos.Name = "tsmiVerCiclos";
+            tsmiVerCiclos.Size = new Size(125, 22);
+            tsmiVerCiclos.Text = "Ver Ciclos";
+            tsmiVerCiclos.Click += tsmiVerCiclos_Click;
+            // 
+            // txmiProfesores
+            // 
+            txmiProfesores.DropDownItems.AddRange(new ToolStripItem[] { tsmiVerProfesores, tsmiModificarTutorias });
+            txmiProfesores.Name = "txmiProfesores";
+            txmiProfesores.Size = new Size(74, 20);
+            txmiProfesores.Text = "Profesores";
+            // 
+            // tsmiVerProfesores
+            // 
+            tsmiVerProfesores.Name = "tsmiVerProfesores";
+            tsmiVerProfesores.Size = new Size(170, 22);
+            tsmiVerProfesores.Text = "Ver Profesores";
+            tsmiVerProfesores.Click += tsmiVerProfesores_Click;
+            // 
+            // tsmiModificarTutorias
+            // 
+            tsmiModificarTutorias.Name = "tsmiModificarTutorias";
+            tsmiModificarTutorias.Size = new Size(170, 22);
+            tsmiModificarTutorias.Text = "Modificar Tutorias";
+            // 
+            // tsmiAlumnos
+            // 
+            tsmiAlumnos.DropDownItems.AddRange(new ToolStripItem[] { tsmiVerAlumnos, añadirAlumnoToolStripMenuItem, modificarAlumnoToolStripMenuItem });
+            tsmiAlumnos.Name = "tsmiAlumnos";
+            tsmiAlumnos.Size = new Size(67, 20);
+            tsmiAlumnos.Text = "Alumnos";
+            // 
+            // tsmiVerAlumnos
+            // 
+            tsmiVerAlumnos.Name = "tsmiVerAlumnos";
+            tsmiVerAlumnos.Size = new Size(171, 22);
+            tsmiVerAlumnos.Text = "Ver Alumnos";
+            tsmiVerAlumnos.Click += tsmiVerAlumnos_Click;
+            // 
+            // añadirAlumnoToolStripMenuItem
+            // 
+            añadirAlumnoToolStripMenuItem.Name = "añadirAlumnoToolStripMenuItem";
+            añadirAlumnoToolStripMenuItem.Size = new Size(171, 22);
+            añadirAlumnoToolStripMenuItem.Text = "Añadir Alumno";
+            // 
+            // modificarAlumnoToolStripMenuItem
+            // 
+            modificarAlumnoToolStripMenuItem.Name = "modificarAlumnoToolStripMenuItem";
+            modificarAlumnoToolStripMenuItem.Size = new Size(171, 22);
+            modificarAlumnoToolStripMenuItem.Text = "Modificar Alumno";
+            // 
+            // tsmiProyectos
+            // 
+            tsmiProyectos.DropDownItems.AddRange(new ToolStripItem[] { verProyectosToolStripMenuItem, añadirProyectosToolStripMenuItem, modificarProyectosToolStripMenuItem });
+            tsmiProyectos.Name = "tsmiProyectos";
+            tsmiProyectos.Size = new Size(71, 20);
+            tsmiProyectos.Text = "Proyectos";
+            // 
+            // verProyectosToolStripMenuItem
+            // 
+            verProyectosToolStripMenuItem.Name = "verProyectosToolStripMenuItem";
+            verProyectosToolStripMenuItem.Size = new Size(175, 22);
+            verProyectosToolStripMenuItem.Text = "Ver Proyectos";
+            verProyectosToolStripMenuItem.Click += verProyectosToolStripMenuItem_Click;
+            // 
+            // añadirProyectosToolStripMenuItem
+            // 
+            añadirProyectosToolStripMenuItem.Name = "añadirProyectosToolStripMenuItem";
+            añadirProyectosToolStripMenuItem.Size = new Size(175, 22);
+            añadirProyectosToolStripMenuItem.Text = "Añadir Proyecto";
+            // 
+            // modificarProyectosToolStripMenuItem
+            // 
+            modificarProyectosToolStripMenuItem.Name = "modificarProyectosToolStripMenuItem";
+            modificarProyectosToolStripMenuItem.Size = new Size(175, 22);
+            modificarProyectosToolStripMenuItem.Text = "Modificar Proyecto";
+            // 
+            // tsmiLogOut
+            // 
+            tsmiLogOut.Name = "tsmiLogOut";
+            tsmiLogOut.Size = new Size(88, 20);
+            tsmiLogOut.Text = "Cerrar Sesion";
+            // 
+            // ltvListaPrincipal
+            // 
+            ltvListaPrincipal.GridLines = true;
+            ltvListaPrincipal.Location = new Point(12, 27);
+            ltvListaPrincipal.MultiSelect = false;
+            ltvListaPrincipal.Name = "ltvListaPrincipal";
+            ltvListaPrincipal.Size = new Size(698, 358);
+            ltvListaPrincipal.TabIndex = 8;
+            ltvListaPrincipal.UseCompatibleStateImageBehavior = false;
+            ltvListaPrincipal.View = View.Details;
+            // 
+            // Principal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(722, 397);
+            Controls.Add(ltvListaPrincipal);
+            Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MainMenuStrip = menuStrip1;
+            Name = "Principal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Perfil de Usuario";
+            Load += Principal_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem tsmiCiclos;
+        private ToolStripMenuItem txmiProfesores;
+        private ToolStripMenuItem tsmiAlumnos;
+        private ToolStripMenuItem tsmiProyectos;
+        private ToolStripMenuItem tsmiLogOut;
+        private ListView ltvListaPrincipal;
+        private ToolStripMenuItem tsmiVerCiclos;
+        private ToolStripMenuItem tsmiVerProfesores;
+        private ToolStripMenuItem tsmiModificarTutorias;
+        private ToolStripMenuItem tsmiVerAlumnos;
+        private ToolStripMenuItem añadirAlumnoToolStripMenuItem;
+        private ToolStripMenuItem modificarAlumnoToolStripMenuItem;
+        private ToolStripMenuItem verProyectosToolStripMenuItem;
+        private ToolStripMenuItem añadirProyectosToolStripMenuItem;
+        private ToolStripMenuItem modificarProyectosToolStripMenuItem;
+    }
+}
---
+++ MiniReto_interfaz_principal/MiniReto_interfaz_principal/Principal.cs
@@ -0,0 +1,81 @@
+namespace MiniReto_interfaz_principal
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsmiVerCiclos_Click(object sender, EventArgs e)
+        {
+            ltvListaPrincipal.Items.Clear();
+            ltvListaPrincipal.Columns.Clear();
+            ltvListaPrincipal.Columns.Add("Codigo", 35);
+            ltvListaPrincipal.Columns.Add("Ciclo", 100);
+            ltvListaPrincipal.Columns.Add("Etapa", 50);
+            ltvListaPrincipal.Columns.Add("Titulo", 100);
+            ltvListaPrincipal.Columns.Add("Curriculo", 100);
+            ltvListaPrincipal.Columns.Add("Familia", 100);
+
+        }
+
+        private void tsmiVerProfesores_Click(object sender, EventArgs e)
+        {
+            ltvListaPrincipal.Items.Clear();
+            ltvListaPrincipal.Columns.Clear();
+            ltvListaPrincipal.Columns.Add("Id", 25);
+            ltvListaPrincipal.Columns.Add("Nombre", 100);
+            ltvListaPrincipal.Columns.Add("Apellidos", 100);
+            ltvListaPrincipal.Columns.Add("Dni", 100);
+            ltvListaPrincipal.Columns.Add("Email", 100);
+            ltvListaPrincipal.Columns.Add("Contraseña", 100);
+            ltvListaPrincipal.Columns.Add("Telefono", 100);
+            ltvListaPrincipal.Columns.Add("Genero", 25);
+            ltvListaPrincipal.Columns.Add("Fecha de Nacimiento", 100);
+            ltvListaPrincipal.Columns.Add("Especialidad", 100);
+            ltvListaPrincipal.Columns.Add("Activo", 25);
+            ltvListaPrincipal.Columns.Add("Admin", 25);
+        }
+
+        private void tsmiVerAlumnos_Click(object sender, EventArgs e)
+        {
+            ltvListaPrincipal.Items.Clear();
+            ltvListaPrincipal.Columns.Clear();
+            ltvListaPrincipal.Columns.Add("Id", 25);
+            ltvListaPrincipal.Columns.Add("Nombre", 100);
+            ltvListaPrincipal.Columns.Add("Apellidos", 100);
+            ltvListaPrincipal.Columns.Add("Dni", 100);
+            ltvListaPrincipal.Columns.Add("Email", 100);
+            ltvListaPrincipal.Columns.Add("Contraseña", 100);
+            ltvListaPrincipal.Columns.Add("Telefono", 100);
+            ltvListaPrincipal.Columns.Add("Genero", 35);
+            ltvListaPrincipal.Columns.Add("Fecha de Nacimiento", 100);
+            ltvListaPrincipal.Columns.Add("Activo", 25);
+            ltvListaPrincipal.Columns.Add("Admin", 25);
+        }
+
+        private void verProyectosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ltvListaPrincipal.Items.Clear();
+            ltvListaPrincipal.Columns.Clear();
+            ltvListaPrincipal.Columns.Add("Id", 35);
+            ltvListaPrincipal.Columns.Add("Nombre", 100);
+            ltvListaPrincipal.Columns.Add("Tipo", 100);
+            ltvListaPrincipal.Columns.Add("Resumen", 100);
+            ltvListaPrincipal.Columns.Add("Año Academico", 100);
+            ltvListaPrincipal.Columns.Add("Fecha Presentacion", 100);
+            ltvListaPrincipal.Columns.Add("Logo", 100);
+            ltvListaPrincipal.Columns.Add("Memoria", 100);
+            ltvListaPrincipal.Columns.Add("Archivos", 100);
+            ltvListaPrincipal.Columns.Add("Comentarios", 100);
+            ltvListaPrincipal.Columns.Add("Ciclo", 35);
+            ltvListaPrincipal.Columns.Add("Tutor", 35);
+        }
+    }
+}
