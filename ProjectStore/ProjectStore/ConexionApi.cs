b143d96584a48b1fa0903cc040296b0e5a5d59fc
unknown <dam2@iesmhp.local>
2024-11-22 18:55:46 +0100

    arreglamos cosas de la  conexion

--- ProjectStore/ProjectStore/ConexionApi.cs
+++ ProjectStore/ProjectStore/ConexionApi.cs
@@ -1,7 +1,4 @@
-﻿using MiniReto_interfaz_principal;
-using Newtonsoft.Json;
-using ProyectoIntermodular.Clases;
-using ProyectoIntermodular.Formularios;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +6,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace ProjectStore
 {
@@ -21,7 +19,7 @@ namespace ProjectStore
             client = new HttpClient();
         }
 
-        public async Task<List<Incidencias>> GetIncidencias()
+    /*    public async Task<List<Incidencias>> GetIncidencias()
         {
             try
             {
@@ -64,29 +62,107 @@ namespace ProjectStore
                 return false;
             }
         }
+    */
 
+        public async Task<List<Profesor>> ProfesorporCorreo(String correo)
+        {
+            try
+            {
+                // Cambiar por  ip de  equipo
 
-        public async Task<Profesor> ObtenerProfesor()
+                // ip 10.0.22.11
+                HttpResponseMessage response = await client.GetAsync($"http://localhost:4000/obtenerProfesoresCorreo{correo}");
+                response.EnsureSuccessStatusCode();
+                string responseJson = await response.Content.ReadAsStringAsync();
+                List<Profesor> profesor = JsonConvert.DeserializeObject<List<Profesor>>(responseJson);
+                return profesor;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Se produjo un error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+        public async Task<List<Profesor>> ObtenerProfesor()
         {
             try
             {
                 // Formar la URL con el ID de la incidencia
                 string url = $"http://localhost:4000/obtenerProfesores";
-
                 HttpResponseMessage response = await client.GetAsync(url);
                 response.EnsureSuccessStatusCode();
-
                 string responseJson = await response.Content.ReadAsStringAsync();
-
-                Profesor profesor = JsonConvert.DeserializeObject<Profesor>(responseJson);
-                return profesor;
+                List<Profesor> profesores = JsonConvert.DeserializeObject<Profesor>(responseJson);
+                return profesores;
             }
             catch (Exception)
             {
                 return null;
             }
         }
+        public async Task<List<Proyecto>> Proyectos()
+        {
+            try
+            {
+                // Cambiar por  ip de  equipo
 
+                // ip 10.0.22.11
+                HttpResponseMessage response = await client.GetAsync($"http://localhost:4000/obtenerProyectos");
+                response.EnsureSuccessStatusCode();
+
+                string responseJson = await response.Content.ReadAsStringAsync();
+
+                List<Proyecto> proyectos = JsonConvert.DeserializeObject<List<Profesor>>(responseJson);
+                return proyectos;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Se produjo un error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+        public async Task<List<Proyecto>> Proyectosid(int id)
+        {
+            try
+            {
+                // Cambiar por  ip de  equipo
+
+                // ip 10.0.22.11
+                HttpResponseMessage response = await client.GetAsync($"http://localhost:4000/obtenerProyectos{id}");
+                response.EnsureSuccessStatusCode();
+
+                string responseJson = await response.Content.ReadAsStringAsync();
+
+                List<Proyecto> proyectos = JsonConvert.DeserializeObject<List<Profesor>>(responseJson);
+                return proyectos;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Se produjo un error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+        public async Task<bool> ActualizarProyecto(int id, Proyecto proyecto)
+        {
+            try
+            {
+                // Convertir el objeto incidencia a formato JSON
+                string jsonData = JsonConvert.SerializeObject(proyecto);
+                StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                // Realizar la solicitud PUT
+                HttpResponseMessage response = await client.PutAsync($"http://localhost:4000/actualizarProyecto{id}", content);
+                response.EnsureSuccessStatusCode();
+
+                // La incidencia se actualizó correctamente
+                return true;
+            }
+            catch (Exception)
+            {
+                // Hubo un error al actualizar la incidencia
+                return false;
+            }
+        }
 
     }
 
