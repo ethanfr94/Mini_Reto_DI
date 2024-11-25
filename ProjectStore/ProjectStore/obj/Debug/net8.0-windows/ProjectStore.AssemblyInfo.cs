0a82636197cc1ac5fad95acfaceeb30b274387c8
ethanfr94 <izan.francoanduaga@iesmiguelherrero.com>
2024-11-25 15:05:34 +0100

    correccion conexion api

--- ProjectStore/ProjectStore/obj/Debug/net8.0-windows/ProjectStore.AssemblyInfo.cs
+++ ProjectStore/ProjectStore/obj/Debug/net8.0-windows/ProjectStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0738a03f0d6a5736462a58ca15f10f578ed4c564")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
